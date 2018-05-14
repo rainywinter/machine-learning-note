de2a5cbfd9544475659a6019342ac51c8265658b
rainywinter <sunny_up_me@163.com>
2018-05-14 10:32:20 +0800

    参考了已有的代码，待回头看明白

--- machine-learning-ex4/ex4/randInitializeWeights.m
+++ machine-learning-ex4/ex4/randInitializeWeights.m
@@ -19,7 +19,8 @@ W = zeros(L_out, 1 + L_in);
 % Note: The first column of W corresponds to the parameters for the bias unit
 %
 
-epsilon = sqrt(6)/sqrt(L_in , L_out);
+%epsilon = sqrt(6)/sqrt(L_in + L_out);
+epsilon = 0.12;
 
 W = rand(L_in,L_out)*(2*epsilon) - epsilon;
 
